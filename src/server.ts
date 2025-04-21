b37f85c24d011f0c6532dd419782ac81b92ddb46
Cleiton H. da Fonseca <113487188+Fonsecach@users.noreply.github.com>
2025-04-20 22:13:58 -0300

    configs

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { env } from "../env";
+import { app } from "./app";
+
+app.listen({
+    port: env.PORT,
+    host: '0.0.0.0'
+}).then(() => {
+	console.log("HTTP server running on http://localhost:3333");
+})
