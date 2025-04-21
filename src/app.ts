d1e04e81183e00d9aeee26ddb3703814e4f19cd1
Cleiton H. da Fonseca <113487188+Fonsecach@users.noreply.github.com>
2025-04-21 18:17:36 -0300

    Update model User, create Gym and CheckIn

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 
 export const app = fastify()
 
+const prisma = new PrismaClient()
+
+prisma.User.create({
+    data: {
+        id: "1",
+        name: "John Doe",
+        email: "exaple@example.com"
+    }
+})
